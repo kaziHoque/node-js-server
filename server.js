018c4ad1646f283f5298e1f4b047b354e7405689
ScarletMcLearn <ratul_shams@yahoo.com>
2018-11-12 09:30:16 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
- var http = require('http');
- var fs = require('fs');
+//  var http = require('http');
+//  var fs = require('fs');
  
-  var server = http.createServer(function(req, res)
-  {
-    res.statusCode = 200;
-    // res.setHeader('Content-Type', 'text/plain');
-    // res.end("Hello World\n");
+//   var server = http.createServer(function(req, res)
+//   {
+//     res.statusCode = 200;
+//     // res.setHeader('Content-Type', 'text/plain');
+//     // res.end("Hello World\n");
     
-        res.setHeader('Content-Type', 'text/html');
-        fs.readFile('index.html', function(err, data){
+//         res.setHeader('Content-Type', 'text/html');
+//         fs.readFile('index.html', function(err, data){
         
-        if(err)
-        {
-            return console.log('File Read Error!');
-        }
+//         if(err)
+//         {
+//             return console.log('File Read Error!');
+//         }
         
-        res.end(data);
+//         res.end(data);
         
-        });
+//         });
         
-  });
-  server.listen(process.env.PORT, process.env.IP, function(){
+//   });
+//   server.listen(process.env.PORT, process.env.IP, function(){
+//     console.log('Server running');
+//   });
+
+
+
+
+
+
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.Server(app);
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname+'/index.html');
+    
+
+    
+});
+
+
+
+
+app.get('/about', function(request, response){
+    response.sendFile(__dirname+'/about.html');
+    
+
+    
+});
+
+
+ server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
