6545bea68940afc01efce73d64a40bc2e275f7e5
ScarletMcLearn <ratul_shams@yahoo.com>
2018-11-14 09:31:11 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -34,6 +34,12 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded:true}));
+
+
 app.get('/', function(request, response){
     response.sendFile(__dirname+'/index.html');
     
@@ -52,6 +58,32 @@ app.get('/about', function(request, response){
 });
 
 
+ app.get('/new-article', function(request, response)
+  {
+      response.sendFile(__dirname+'/form.html');
+  });
+  
+ 
+ var article = {} 
+  
+  app.post('/api/article/create', function(request, response)
+  {
+      console.log(request.body);
+      if(!request.body.title){
+         
+          return response.status(400).json({error: 'PLease add a title!'});
+      }
+      response.status(200).json({message: 'Article Created!'});
+  });
+  
+
  server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
+    
   });
+  
+  
+  
+  
+  
+ 
